1ab37c4043964fa6afa45787357a850d84fc8f3f
Anderson <anderson@archlinux.com.br>
2021-05-26 16:06:50 -0300

    Configuração JWT
    Nessa etapa foi realizada a configuração do JWT e em sequencia o ajuste no controller para o primeiro teste de requisição de login via Postman

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $credenciais = $request->all('email','password');
+        $token = auth('api')->attempt($credenciais);
+        //dd($token);
+        
+        if($token){
+            return response()->json(['token' => $token]);
+        } else {
+            return response()->json(['erro' => 'Usuário ou senha inválidos!'], 403);
+        }
+    }
+
+    public function logout(){
+        return 'logout';
+    }
+
+    public function refresh(){
+        return 'refresh';
+    }
+
+    public function me(){
+        return 'me';
+    }
+}
