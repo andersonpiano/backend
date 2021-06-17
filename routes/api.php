fe531f3878ac32e7e12653af67ae65ae352190ea
Anderson <anderson@archlinux.com.br>
2021-06-17 09:28:02 -0300

    ajustes

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('DOK-v1')->middleware('jwt.auth')->group(function () {
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+
+});
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+
 
